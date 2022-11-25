664bf9d700257212b8f4d157ae25cecdbd6c2c0a
plebey <mixail.card@gmail.com>
2022-11-25 20:23:50 +0300

    changed pics of devs + changed colors

--- SonyaSTRANITSA/shablon/assets/js/app.js
+++ SonyaSTRANITSA/shablon/assets/js/app.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', e => {
 	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
 })
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
+/*ScrollSmoother.create({
 	wrapper: '.wrapper',
 	content: '.content1'
-})
+})*/
