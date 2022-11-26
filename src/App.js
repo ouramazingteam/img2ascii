a7fefbb7eda9ba64a15ea03b5380b60b964bf1d6
Sertyhopss <sertyhopss@gmail.com>
2022-11-26 16:06:52 +0300

    some stylization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {Transformer} from "./TransformLogic/Transformer";
 
--- src/TransformLogic/Transformer.js
+++ src/TransformLogic/Transformer.js
@@ -1,10 +1,12 @@
 import React, {useState} from "react";
 import {useForm} from "react-hook-form";
+import {Button, ButtonGroup, Checkbox, Input, Paper, TextField} from "@mui/material";
 
 
 export function Transformer() {
     const {register, handleSubmit, formState: {errors}} = useForm();
     const [text, setText] = useState('')
+    const [visible, setVisible] = useState(false)
 
     const onSubmit = async (data) => {
         const formData = new FormData();
@@ -23,6 +25,7 @@ export function Transformer() {
             body: formData,
         }).then((res) => res.json());
         setText(res['art']);
+        setVisible(true);
     }
 
 
@@ -37,23 +40,44 @@ export function Transformer() {
     return (
         <div>
             <form id='user-input' onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <input type="file" {...register("image", {required: true})}></input>
+                <div style={{display: "block ruby"}}>
+                    <Paper elevation={5} style={{width: '30vw', padding: '1vh', margin: '3vh'}}>
+                        <div>
+                            <h2>Image -> ASCII</h2>
+                        </div>
+                        <div style={{justifyContent: "left"}}>
+                            <div style={{margin: "2vh"}}>
+                                <Input type="file" {...register("image", {required: true})}></Input>
+                            </div>
+                            <div style={{margin: "2vh"}}>
+                                <TextField label="Масштаб" placeholder="0.25" size="small" {...register("scale")}/>
+                            </div>
+                            <div style={{margin: "2vh"}}>
+                                <TextField label="Распределение" placeholder="fill"
+                                           size="small" {...register("distrType")}/>
+                            </div>
+                            <div style={{margin: "2vh"}}>
+                                Инвертировать <Checkbox {...register("invert")}/>
+                            </div>
+                            <div style={{margin: '5px'}}>
+                                <ButtonGroup size="large" aria-label="large button group">
+                                    <Button variant="contained" type="submit">Генерация</Button>
+                                    <Button variant="contained"
+                                            onClick={() => copy_to_clipboard(text)}>Скопировать</Button>
+                                </ButtonGroup>
+                            </div>
+                        </div>
+                    </Paper>
                 </div>
-                <div>
-                    Scale: <input type="text" placeholder="0.25" {...register("scale")}/>
-                </div>
-                <div>
-                    Distr_type: <input type="text" placeholder="fill" {...register("distrType")}/>
-                </div>
-                <div>
-                    Invert: <input type="checkbox" {...register("invert")}/>
-                </div>
-                <div>
-                    <button type="submit">Ok</button>
-                    <button onClick={() => copy_to_clipboard(text)}>Copy</button>
-                </div>
-                <textarea value={text} spellCheck="false" style={{width: "1000px", height: "1000px", fontSize: "7px", backgroundColor: "#112", color: "#bbb"}}/>
+                {visible ? <textarea value={text} spellCheck="false" style={{
+                        width: "1000px",
+                        height: "1000px",
+                        fontSize: "7px",
+                        backgroundColor: "#112",
+                        color: "#bbb",
+                        margin: "5vh",
+                    }}/>
+                    : null}
             </form>
         </div>
     )
